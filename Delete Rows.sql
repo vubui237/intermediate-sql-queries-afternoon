a88cb78597789db4eb16f646be5998795ab736bb
vubui237 <vtb0977@gmail.com>
2017-08-08 15:06:41 -0500

    Delets

---
+++ Delete Rows.sql
@@ -0,0 +1,22 @@
+--1.
+-- CREATE TABLE practice_delete ( Name string, Type string, Value integer );
+-- INSERT INTO practice_delete ( Name, Type, Value ) VALUES ("delete", "bronze", 50);
+-- INSERT INTO practice_delete ( Name, Type, Value ) VALUES ("delete", "bronze", 50);
+-- INSERT INTO practice_delete ( Name, Type, Value ) VALUES ("delete", "bronze", 50);
+-- INSERT INTO practice_delete ( Name, Type, Value ) VALUES ("delete", "silver", 100);
+-- INSERT INTO practice_delete ( Name, Type, Value ) VALUES ("delete", "silver", 100);
+-- INSERT INTO practice_delete ( Name, Type, Value ) VALUES ("delete", "gold", 150);
+-- INSERT INTO practice_delete ( Name, Type, Value ) VALUES ("delete", "gold", 150);
+-- INSERT INTO practice_delete ( Name, Type, Value ) VALUES ("delete", "gold", 150);
+-- INSERT INTO practice_delete ( Name, Type, Value ) VALUES ("delete", "gold", 150);
+
+-- SELECT * FROM practice_delete;
+
+--2,
+-- DELETE FROM practice_delete WHERE Type="bronze"
+
+--3.
+-- DELETE FROM practice_delete WHERE Type="silver"
+
+--4.
+-- DELETE FROM practice_delete WHERE Value=150;
